4795420910ed92ff9ed2de99b45b2513756b7a47
Luis Rey Lara Gonzalez <48841140+luisreylaragonzalez@users.noreply.github.com>
2021-08-04 10:46:51 -0500

    Update ServidorWeb3.ino
    password

--- ServidorWeb3.ino
+++ ServidorWeb3.ino
@@ -2,8 +2,8 @@
 // Enciende y apaga LED. Botones. Imágenes.
 
 #include <WiFi.h>
-const char* ssid = "Estoicos";
-const char* password = "*Andromeda_$23";
+const char* ssid = "";
+const char* password = "";
 
 WiFiServer server(80);
 
