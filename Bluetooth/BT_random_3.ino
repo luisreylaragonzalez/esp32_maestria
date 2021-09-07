aaf7b98f45575ac0137851af916fd4503392ec1c
Luis Rey Lara Gonzalez <48841140+luisreylaragonzalez@users.noreply.github.com>
2021-09-07 10:17:15 -0500

    Add files via upload
    randpm

---
+++ Bluetooth/BT_random_3.ino
@@ -0,0 +1,30 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+int aleatorio;
+String alea;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+    aleatorio = random(0, 10000000);
+    alea = (String) aleatorio + "\n";
+    SerialBT.print(alea); // Debe ser String y con \n.
+  
+
+  delay(2000);
+}
